981e91b1f2be9b0f2093917cce8d7755f30a3999
John U. Balis <phobossystems@gmail.com>
2023-10-15 00:06:40 -0500

    refactor, added goalie training code

---
+++ AbstractSimAttacker/README.md
@@ -0,0 +1,123 @@
+# BadgerRLSystem 2023 CodeRelease
+
+This repository contains the code used to train the attacker policy used by BadgerBots in Robocup 2023. 
+We are still getting the training code ready to be easy to run locally.
+
+## Pretrained Policies
+
+Pre-trained Policies are available in the `attacker/exported_policy` directory. They need some manual changes to work with BadgerRLSystemCodeRelease2023; we will fix this soon.
+
+<!---
+## Tools
+
+
+### Trying out a policy
+
+You can try out a policy with the PushBallToGoalEnv environment for example with the following command
+
+````
+python3 ./envs/push_ball_to_goal.py <path to models folder for policy>
+
+specifically
+python3 ./envs/push_ball_to_goal.py /Models/push_ball_to_goal
+
+````
+
+### export_model.py
+
+export_model.py can be used to export a stable-baselines3 saved policy into a format that can be read by the C++ code base. 
+
+When export_model.py is executed, it will create action_policy.h5, vale_policy.h5, shared_policy.h5, and metadata.json. All of these files need to be moved
+to the Config directory of the C++ repository to try them out in the c++ environments. 
+
+Note that export model needs both the vector normalize json and the policy file specified. 
+
+
+### analyze_trajectories.py
+
+This is provided for checking the similarity between the action_means recorded from the C++ policy during rollouts, and the 
+action mean predictions made by the stable-baselines3 policy, and the converted keras models. An example trajectory file is provided in  the Examples directory. 
+
+### generate_baselines.py
+
+This allows the baselines to be automatically trained. In the future, the abstract environemnts will be instrumented with psuedorandom seeding, so this process should be possible to exactly reproduce. Right now, there is some variation in the quality of the policies. 
+
+To generate a policy run:
+````
+python3 generate_baselines.py <name of policy>
+
+for example:
+python3 generate_baselines.py push_ball_to_goal
+````
+
+
+## environment
+````
+python3.9 -m venv env
+source env/bin/activate
+python3 -m pip install -r requirements.txt
+
+````
+
+It is recommended to make a pythonvirtual env using requirements.txt before using the code in this repository, this will ensure you can load the saved policies, and ensure the 
+policies you train are compatible with other people's environments.  You will need to use python 3.9.
+
+## Running tests
+
+A limited test battery is provided with this project (particularly for logic involving trigonometric calculations) To run the tests, make sure you have the virtual environment set up, then execute the command:
+
+````
+python3 -m pytest
+````
+
+More tests are always welcome!
+
+# MARL
+To start, it is convenient to be in the multi_agent directory so 
+````
+cd multi_agent
+````
+
+From there the file ppo_sb3.py is the file used for training and rendering.
+All envs are held in their own folder with the structure ./name_of_env/name_of_env.py
+Under this folder is also a place to store policies relating to this env for ease of use.
+
+
+## To train policies
+**Flags**
+
+--train -> use to set in training mode
+
+--env=<NAME_OF_ENV> -> use to set name of ENV to train. NOTE: Use NAME not PATH 
+
+--continuous_actions=<bool> -> Use to set if using continuous actions (default True)
+  
+--total_timesteps=<x> -> Set number of training timesteps (Default 10 million)
+  
+--batch_size=<x> -> Set batch size (Default 64)
+  
+--vec_envs=<x> -> Set number of SB3 vectorized envs (Default 12)
+
+--wandb -> Flag to use weights and biases to track. Must have wandb initialized to use
+  
+Example of how to train a policy
+````
+python ppo_sb3.py --train --env=kick_ball_to_goal --total_timesteps=10000 --batch_size=1024 --wandb
+````
+
+
+## To test/visualize
+To render policies the env must be specified and the path to the policy to be rendered.
+  
+For example
+````
+python ppo_sb3.py --render --env=role_kick --policy_name=role_kick/ppo_sb3_role_kick 
+````
+  
+--render used to set to render mode
+  
+--env used to choose env to render with
+  
+--policy_name used to specify the path to the zip file created when training (no .zip at the end)
+
+-->
---
+++ AbstractSimAttacker/run.py
@@ -0,0 +1,179 @@
+import time
+import numpy as np
+from stable_baselines3 import PPO
+from stable_baselines3.ppo import MlpPolicy
+from stable_baselines3.common.vec_env import VecMonitor
+from stable_baselines3.common.vec_env import VecNormalize
+
+import supersuit as ss
+import argparse
+import importlib
+
+from wandb.integration.sb3 import WandbCallback
+import wandb
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Running options
+    parser.add_argument('--train', action='store_true', default=False)
+    parser.add_argument('--render', action='store_true', default=False)
+    parser.add_argument('--continue_training', action='store_true', default=False)
+
+    parser.add_argument('--prev_policy', type=str, default=None)
+
+    # Env options
+    parser.add_argument('--env', type=str, default=None)
+    parser.add_argument('--continuous_actions', type=bool, default=True)
+
+    # Training options
+    parser.add_argument('--total_timesteps', type=int, default=10000000)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--vec_envs', type=int, default=8)
+
+    # Wandb options
+    parser.add_argument('--wandb', action='store_true', default=False)
+    # Callback options
+    parser.add_argument('--annealing', action='store_true', default=False)
+
+    # Condor options
+    parser.add_argument('--condor_name', type=str, default=None)
+
+    # Render options
+    parser.add_argument('--policy_name', type=str, default=None)
+
+    if not parser.parse_args().env and not parser.parse_args().condor_name:
+        raise ValueError("Must specify env")
+
+    return parser.parse_args()
+
+def train(args):
+    if args.condor_name:
+        from env import env
+        args.env = args.condor_name
+    else:
+        path = args.env + '.' + args.env
+        env = importlib.import_module(path).env
+
+    run_name = args.env + '_' + time.strftime("%d_%H:%M")
+
+    if args.wandb:
+        run = wandb.init(
+            project=args.env,
+            config={
+                "env": args.env,
+                "total_timesteps": args.total_timesteps,
+                "batch_size": args.batch_size,
+            },
+            sync_tensorboard=True,
+            name=run_name,
+        )
+
+    env = env(env_type='parallel', continuous_actions=args.continuous_actions)
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+
+    env = ss.concat_vec_envs_v1(env, args.vec_envs, base_class='stable_baselines3')
+    env = VecMonitor(env)
+
+    env = VecNormalize(env, norm_obs=False, norm_reward=True, clip_obs=10)
+
+    model = PPO(MlpPolicy, env, batch_size=args.batch_size, 
+                ent_coef=0.01,
+                tensorboard_log=f"runs/{run_name}",
+                verbose=1)
+    
+    if args.wandb:
+        model.learn(total_timesteps=args.total_timesteps, callback=WandbCallback(verbose=2))
+    else:
+        model.learn(total_timesteps=args.total_timesteps)
+
+
+    model.save("ppo_sb3_" + args.env)
+
+    if args.wandb:
+        run.finish()
+
+def render(args):
+    # Load model
+    if args.policy_name:
+        model = PPO.load(args.policy_name)
+    else:
+        model = PPO.load("ppo_sb3_" + args.env)
+
+    path = args.env + '.' + args.env
+    env = importlib.import_module(path).env
+
+    # Create environment
+    env = env(env_type='parallel', continuous_actions=True)
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+
+    # Run model
+    obs = env.reset()
+    while True:
+        action, _ = model.predict(obs, deterministic=True)
+        obs, _, _, _, _ = env.step(action)
+        env.render()
+
+def continue_training(args):
+    if args.condor_name:
+        from env import env
+        args.env = args.condor_name
+    else:
+        path = args.env + '.' + args.env
+        env = importlib.import_module(path).env
+
+    run_name = args.env + '_' + time.strftime("%d_%H:%M")
+
+    if args.wandb:
+        run = wandb.init(
+            project=args.env,
+            config={
+                "env": args.env,
+                "total_timesteps": args.total_timesteps,
+                "batch_size": args.batch_size,
+            },
+            sync_tensorboard=True,
+            # monitor_gym=True,
+            name=run_name,
+        )
+
+    env = env(env_type='parallel', continuous_actions=args.continuous_actions)
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+
+    env = ss.concat_vec_envs_v1(env, args.vec_envs, base_class='stable_baselines3')
+    env = VecMonitor(env)
+
+    env = VecNormalize(env, norm_obs=False, norm_reward=True, clip_obs=10)
+
+    # model = PPO(MlpPolicy, env, batch_size=args.batch_size, 
+    #             ent_coef=0.01,
+    #             tensorboard_log=f"runs/{run_name}",
+    #             verbose=1)
+    
+    # Load model.zip in current directory
+    model = PPO.load(args.prev_policy)
+    model.set_env(env)
+    
+    if args.wandb:
+        model.learn(total_timesteps=args.total_timesteps,
+                    callback=WandbCallback(
+                        verbose=2,
+                    )
+        )
+    else:
+        model.learn(total_timesteps=args.total_timesteps)
+
+    model.save("ppo_sb3_" + args.env + "_cont")
+
+    if args.wandb:
+        run.finish()
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    if args.train:
+        train(args)
+    if args.render:
+        render(args)
+    if args.continue_training:
+        continue_training(args)
